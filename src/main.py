493618ce17ac0602e88bc7c7b162f0ce0cde3714
jaemyung.kim <jaemyung.kim@navercorp.com>
2023-05-01 21:44:07 +0900

    add shutdown event

--- src/main.py
+++ src/main.py
@@ -9,7 +9,15 @@ app = FastAPI()
 app.include_router(demo.router)
 
 
+# https://fastapi.tiangolo.com/advanced/events/
 @app.on_event("startup")
 def startup():
     repo = container[Repo]
     repo.show()
+
+
+def shutdown_handler():
+    print('server is shutdown')
+
+
+app.on_event('shutdown')(shutdown_handler)
